288977b505dce1d4247033fecae0f37b176f5929
ACTony47 <68453250+ACTony47@users.noreply.github.com>
2024-01-06 14:37:29 +0800

    new codes

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
-public class test {
+//
+//@author : actony47
+//
+public class Test {
     public static void main(String []args){
         System.out.println("hello, world");
+        System.out.println("hello,world");
     }
 }

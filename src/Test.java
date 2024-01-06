153c9221e0cde4cca7fd092a101764e83a654dc1
ACTony47 <68453250+ACTony47@users.noreply.github.com>
2024-01-06 14:57:03 +0800

    new new codes

--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String []args){
         System.out.println("hello, world");
         System.out.println("hello,world");
+        System.out.println("i don't know why i git pull this and it showed successful but codes were not updated");
     }
 }
